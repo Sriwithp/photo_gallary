23a9320498980cf57eed6daaf8d4063ca879f514
Sriramu P | sriwithp <Sriwithp@gmail.com>
2023-10-04 18:12:09 +0530

    commit

---
+++ lib/fullscreen_image.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:pinch_zoom/pinch_zoom.dart';
+
+class FullScreenImagePage extends StatelessWidget {
+  final List<String> imagePaths;
+  final int currentIndex;
+
+  const FullScreenImagePage({super.key, 
+    required this.imagePaths,
+    required this.currentIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Viewer'),
+      ),
+      body: PageView.builder(
+        itemCount: imagePaths.length,
+        controller: PageController(initialPage: currentIndex),
+        itemBuilder: (context, index) {
+          final imagePath = imagePaths[index];
+          return GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: PinchZoom(
+              resetDuration: const Duration(milliseconds: 100),
+              maxScale: 2.5,
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.contain,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'fullscreen_image.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<String> images = [
+    "assets/img1.jpg",
+    "assets/img2.jpg",
+    "assets/img3.jpg",
+    "assets/img4.jpg",
+    "assets/img5.jpg",
+    "assets/img6.jpg",
+    "assets/img7.jpg",
+    "assets/img1.jpg",
+    "assets/img2.jpg",
+    "assets/img3.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Image Gallary"),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                ),
+                itemCount: images.length,
+                itemBuilder: (context, index) {
+                  final imagePath = images[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FullScreenImagePage(
+                            imagePaths: images,
+                            currentIndex: index,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Hero(
+                      tag: "img$index",
+                      child: Image.asset(
+                        "assets/img${index + 1}.jpg",
+                        fit: BoxFit.cover,
+                        width: 150,
+                        height: 150,
+                      ),
+                    ),
+                  );
+                })));
+  }
+}
